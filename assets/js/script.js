c700cf8f60f043ddafceaeb0d833836d4955f1a5
Sirinya Richardson <sirinya@lemonbrain.ch>
2022-12-26 09:40:27 +0100

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", () => {
+    let buttons = document.getElementsByTagName('button')
+
+    /*
+    Arrow functions - Arrow functions have no concept of this.
+    this inside of an arrow function is whatever this is in their containing lexical environment.
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+    Don't use arrow functions if you need to bind the value of this
+
+    !! Does not work
+     for (let button of buttons) {
+        button.addEventListener('click', () => {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!")
+            }
+        })
+    }
+    */
+
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!")
+            } else {
+                let gameType = this.getAttribute('data-type')
+                alert(`You clicked ${gameType}`)
+            }
+        })
+    }
+})
+
 function runGame() {
 }
 
