83173974c5cc159ad18af46e24cf1a3a05e14ef9
Sirinya Richardson <sirinya@lemonbrain.ch>
2022-12-26 09:53:54 +0100

    Display addiion question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,20 +25,29 @@ document.addEventListener("DOMContentLoaded", () => {
                 alert("You clicked submit!")
             } else {
                 let gameType = this.getAttribute('data-type')
-                alert(`You clicked ${gameType}`)
+                runGame(`${gameType}`)
             }
         })
     }
+
+    runGame('addition')
 })
 
 /*
 * The main game "loop", called when the script is first loaded
 * and after the user's answer has been processed
 */
-function runGame() {
+function runGame(gameType) {
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1
     let num2 = Math.floor(Math.random() * 25) + 1
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`Unknown game type: ${gameType}`)
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -53,7 +62,11 @@ function incrementScore() {
 function incrementWrongAnswer() {
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    // Interrogate HTML
+    document.getElementById('operand-1').textContent = operand1
+    document.getElementById('operand-2').textContent = operand2
+    document.getElementById('operator').textContent = "+"
 }
 
 function displaySubtractQuestion() {
